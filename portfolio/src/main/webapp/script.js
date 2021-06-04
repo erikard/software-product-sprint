9bafcc884aeffa8c3ca26148bcec40463c5f6e3c
Rix <erikardiaz7@gmail.com>
2021-06-04 05:10:44 +0000

    Update the  README.md file to describe the repo.Also added some walkthrough changes to portfolio.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,16 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random statement to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomStatement() {
+  const statements =
+      ['The only person I can control... is myself.', 'Remember to smile!', 'Some people only talk to you, so that they can talk about you. Don\'t give them that power!', 'Life is beautiful :)'];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  // Pick a random statement.
+  const statement = statements[Math.floor(Math.random() * statements.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const statementContainer = document.getElementById('statement-container');
+  statementContainer.innerText = statement;
 }
